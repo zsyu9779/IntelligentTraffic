a93d92ffeb01a634cdace7972e41621c5c8908cf
zsy <zhangshiyu01@do-global.com>
2021-03-10 17:23:06 +0800

    add model

---
+++ src/main/java/com/manager/traffic/entity/CarInfo.java
@@ -0,0 +1,18 @@
+package com.manager.traffic.entity;
+
+import lombok.Data;
+
+
+@Data
+public class CarInfo {
+    private Long engineNumber;
+    private String plateNumber;
+    private String plateType;
+    private String master;
+    private String masterId;
+    private String carBrand;
+    private String carColor;
+    private String phoneNumber;
+
+
+}
